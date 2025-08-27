db8581d70373a531e02accd83630661b635cdad2
Kaushalya193 <e19193@eng.pdn.ac.lk>
2025-08-27 20:42:04 +0530

    add java file

---
+++ test/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        // int total=0;
+        // int k=0;
+        // for (int i=0;i<s.length();i++){
+        //     int count;
+        //     for (int j=k;j<i;j++){
+        //         if (s.charAt(i)==s.charAt(j)){
+        //             k=j+1;
+        //             break;
+        //         }
+               
+        //     }
+        //      count=i-k+1;
+        //      if (count>total){
+        //         total=count;
+        //     }
+        
+        // }   
+        // return total;     
+        HashMap <character, integer> lastseen = new HashMap<>();
+    }
+}
